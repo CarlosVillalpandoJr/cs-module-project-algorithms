36c39d1d79b20ad1b88e7cbddb5fcc1fb68e620f
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-05-21 14:52:06 -0500

    completed single number problem

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,24 @@
 '''
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
+
+Notes
+- Have to keep track of how many times a number shows up in the array
+    + Use a dictionary with the [key] being the number the showed up
+        - value can be set to the number of occurences
+- Set up an if statement
+    + something like, if num is not in dict return num
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
+    nums = {}
+    for num in arr:
+        if num in nums:
+            nums[num] += 1 
+        else:
+            nums[num] = 1
+    for num in nums:
+        if nums[num] == 1:
+            return num
 
 
 if __name__ == '__main__':
